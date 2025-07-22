2d9ca1aeaeb583031561750b0fb252bdbf889bd2
Joaoacl <joao_edu_ribeiro@outlook.com>
2025-07-22 00:17:47 -0300

    first commit

---
+++ config.js
@@ -0,0 +1,17 @@
+/**
+ * ====================================================================
+ * CONFIGURAÇÃO DO CARDÁPIO DIGITAL - V3.2 - 17/07/2025
+ * 
+ */
+
+// URL of the spreadsheet with menu items (tab: Items)
+const MENU_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ0Sl9QPASN0rLtJTUCEmDbTg7p8L67blBj9d8ZbMRDzdv5E_cPiuFVgPNrRoq6hQ/pub?gid=1619056610&single=true&output=csv';
+
+// URL of the spreadsheet with menu categories (tab: Categories)
+const CATEGORIES_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ0Sl9QPASN0rLtJTUCEmDbTg7p8L67blBj9d8ZbMRDzdv5E_cPiuFVgPNrRoq6hQ/pub?gid=1922196281&single=true&output=csv';  
+
+// URL of the spreadsheet with operating hours (tab: Hours)
+const HOURS_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ0Sl9QPASN0rLtJTUCEmDbTg7p8L67blBj9d8ZbMRDzdv5E_cPiuFVgPNrRoq6hQ/pub?gid=1083573357&single=true&output=csv';
+
+// URL of the spreadsheet with general settings (tab: Config)
+const CONFIG_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ0Sl9QPASN0rLtJTUCEmDbTg7p8L67blBj9d8ZbMRDzdv5E_cPiuFVgPNrRoq6hQ/pub?gid=1930677213&single=true&output=csv';
